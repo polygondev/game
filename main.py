bd57f826df4dc3bc37ef729c1480926c33396076
Polygon <66850658+polygondev@users.noreply.github.com>
2023-05-29 10:05:45 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 points = 0
 print("Welcome to GuessTheLyrics")
 name = input("What is your name?")
-print("Hello", name "\n")
+print("Hello", name)
 
 input("Press the Enter key to continue: ")
 
-input("Choose a song:\n")
+print("Level 1")
+input("Choose a song:\nOption 1: Bohemian Rhapsody\nOption 2: Wrecking Ball")
