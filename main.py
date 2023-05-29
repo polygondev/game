99f2b9eab35f41596f7ba39a47823b6411976709
Zoltblock <115430246+Zoltblock@users.noreply.github.com>
2023-05-29 12:35:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ print("Use the number until we introduce text support")
 lv1 = int(input("Choose a song:\nOption 1: Bohemian Rhapsody\nOption 2: Wrecking Ball"))
 
 if lv1 in [1, "Bohemian Rhapsody", "Option 1"]:
-    lv1s1 = str(("Guess the missing lyrics:\nMama, just ______ a man"))
-    if lv1s1 == killed:
+    lv1s1 = input(("Guess the missing lyrics:\nMama, just ______ a man"))
+    if lv1s1 == "killed":
+        print("correct")
+
+if lv1 in [2, "Wreakingball", "Option 2"]:
+    lv1s1 = input(("Guess the missing lyrics:\nI came in ___ a ___"))
+    if lv1s1 == "like wreakingball":
         print("correct")
