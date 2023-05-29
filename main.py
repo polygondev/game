f59de1bd0a0e49fdda3a63cca1b11f37032dbfda
Polygon <66850658+polygondev@users.noreply.github.com>
2023-05-29 13:14:55 +1000

    Working levels. Screen also clears.

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
-# Working
- 
+# Import
+from os import system, name
+from time import sleep
+
+# Define Variables and Functions
 points = 0
 print("Welcome to GuessTheLyrics")
-#name = input("What is your name")
-#print("Hello", name)
 
-#input("Press the Enter key to continue: ")
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+def nextlevel():
+    clear()
+    print("You have", points, "points")
 
 print("Level 1")
-
-# Broken
 lv1 = str(input("Choose a song:\nOption 1: Bohemian Rhapsody\nOption 2: Wrecking Ball\n"))
 
-#if lv1 == 1:
-#    lv1s1 = str(("Guess the missing lyrics:\nMama, just ______ a man"))
-#else:
-#    print()
-#    if lv1s1 == killed:
-#        print("correct")
+game = True
+while game:
+    level1 = True
+    while level1:
+        if lv1 in ["1", "Bohemian Rhapsody", "Option 1"]:
+            lv1a1 = str(input("What are the missing lyrics:\n'Mama, just ______ a man'?\n"))
+            if lv1a1 in ["killed", "Killed", "KILLED", "killed.", "Killed."]:
+                print("Correct!")
+                points += 1
+                level1 = False
+                
+            else:
+                print("Incorrect.")
+                level1 = False
+                game = False
+        elif lv1 in ["2", "Wrecking Ball", "Option 2"]:
+            lv1a1 = str(input("What are the missing lyrics:\n'Mama, just ______ a man'?\n"))
+            if lv1a1 in ["killed", "Killed", "KILLED", "killed.", "Killed."]:
+                print("Correct!")
+                points += 1
+                level1 = False
+        else:
+            print("Invalid option, please try again.")
+    level2 = True
+    nextlevel()
 
-if lv1 in ["1", "Bohemian Rhapsody", "Option 1"]:
-    print("working")
+    while level2:
+        print("Working.")
+        sleep(20)
+        
+print("Game Over. Your score was", points)
